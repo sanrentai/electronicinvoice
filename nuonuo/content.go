c3ffb4acf7b49f279ddbd89625e9b9d21d02a9fe
sanrentai <sanrentai@163.com>
2023-05-10 10:44:06 +0800

    更新诺诺发票明细

--- nuonuo/content.go
+++ nuonuo/content.go
@@ -105,29 +105,35 @@ type Order struct {
 	// realPropertyRentInfo	Object	N			开具不动产经营租赁服务特定要素的数电票才需要传（specificFactor 为 6时）；注：此时 商品只能有 一条明细 且 规格型号、单位都不能有值
 	// realPropertySellInfo	Object	N			开具不动产销售特定要素的数电票才需要传（specificFactor 为 5时）；注：此时 商品只能有 一条明细 且 规格型号、单位都不能有值
 	// differenceVoucherInfoList	Array	N			数电发票差额扣除凭证列表，开具数电差额征税-差额开票时，必传
-	// additionalElementName	String	N	测试模版	50	附加模版名称（数电电票特有字段，附加模版有值时需要添加附加要素信息列表对象，需要先在电子税局平台维护好模版）
+	// additionalElementName	string //	N	测试模版	50	附加模版名称（数电电票特有字段，附加模版有值时需要添加附加要素信息列表对象，需要先在电子税局平台维护好模版）
 	// additionalElementList	Array	N		10	附加要素信息列表（数电电票特有字段，附加要素信息可以有多个，有值时需要附加模版名称也有值）
 
 }
 
 type InvoiceDetail struct {
-	Deduction           string `json:"deduction,omitempty"`
-	FavouredPolicyFlag  string `json:"favouredPolicyFlag,omitempty"`
-	FavouredPolicyName  string `json:"favouredPolicyName,omitempty"`
-	GoodsCode           string `json:"goodsCode,omitempty"`
-	GoodsName           string `json:"goodsName"` // Y 商品名称（如invoiceLineProperty =1，则此商品行为折扣行，折扣行不允许多行折扣，折扣行必须紧邻被折扣行，商品名称必须与被折扣行一致）
-	InvoiceLineProperty string `json:"invoiceLineProperty,omitempty"`
-	Num                 string `json:"num,omitempty"`
-	Price               string `json:"price,omitempty"`
-	SelfCode            string `json:"selfCode,omitempty"`
-	SpecType            string `json:"specType,omitempty"`
-	Tax                 string `json:"tax,omitempty"`
-	TaxExcludedAmount   string `json:"taxExcludedAmount,omitempty"`
-	TaxIncludedAmount   string `json:"taxIncludedAmount,omitempty"`
-	TaxRate             string `json:"taxRate"` // Y 税率，注：纸票清单红票存在为null的情况
-	Unit                string `json:"unit,omitempty"`
-	WithTaxFlag         string `json:"withTaxFlag"` // Y 单价含税标志：0:不含税,1:含税
-	ZeroRateFlag        string `json:"zeroRateFlag,omitempty"`
+	GoodsName           string `json:"goodsName,omitempty"`           //	Y	电脑	90	商品名称（如invoiceLineProperty =1，则此商品行为折扣行，折扣行不允许多行折扣，折扣行必须紧邻被折扣行，商品名称必须与被折扣行一致）
+	GoodsCode           string `json:"goodsCode,omitempty"`           //	N	1090511030000000000	19	商品编码（商品税收分类编码开发者自行填写）
+	SelfCode            string `json:"selfCode,omitempty"`            //	N	130005426000000000	16	自行编码（可不填）
+	WithTaxFlag         string `json:"withTaxFlag,omitempty"`         //	Y	1	1	单价含税标志：0:不含税,1:含税
+	Price               string `json:"price,omitempty"`               //	N		16	单价（精确到小数点后8位），当单价(price)为空时，数量(num)也必须为空；(price)为空时，含税金额(taxIncludedAmount)、不含税金额(taxExcludedAmount)、税额(tax)都不能为空
+	Num                 string `json:"num,omitempty"`                 //	N		16	数量（精确到小数点后8位，开具红票时数量为负数）
+	Unit                string `json:"unit,omitempty"`                //	N	台	20	单位
+	SpecType            string `json:"specType,omitempty"`            //	N	y460	40	规格型号
+	Tax                 string `json:"tax,omitempty"`                 //	N	0.12	16	税额（精确到小数点后2位），[不含税金额] * [税率] = [税额]；税额允许误差为 0.06。红票为负。不含税金额、税额、含税金额任何一个不传时，会根据传入的单价，数量进行计算，可能和实际数值存在误差，建议都传入
+	TaxRate             string `json:"taxRate,omitempty"`             //	Y	0.13	10	税率，注：1、纸票清单红票存在为null的情况；2、二手车发票税率为null或者0
+	TaxExcludedAmount   string `json:"taxExcludedAmount,omitempty"`   //	N	0.88	16	不含税金额（精确到小数点后2位）。红票为负。不含税金额、税额、含税金额任何一个不传时，会根据传入的单价，数量进行计算，可能和实际数值存在误差，建议都传入
+	TaxIncludedAmount   string `json:"taxIncludedAmount,omitempty"`   //	N	1	16	含税金额（精确到小数点后2位），[不含税金额] + [税额] = [含税金额]，红票为负。不含税金额、税额、含税金额任何一个不传时，会根据传入的单价，数量进行计算，可能和实际数值存在误差，建议都传入
+	InvoiceLineProperty string `json:"invoiceLineProperty,omitempty"` //	N	0	1	发票行性质：0,正常行;1,折扣行;2,被折扣行，红票只有正常行
+	FavouredPolicyFlag  string `json:"favouredPolicyFlag,omitempty"`  //	N	0	2	优惠政策标识：0,不使用;1,使用; 数电票时： 01：简易征收 02：稀土产品 03：免税 04：不征税 05：先征后退 06：100%先征后退 07：50%先征后退 08：按3%简易征收 09：按5%简易征收 10：按5%简易征收减按1.5%计征 11：即征即退30% 12：即征即退50% 13：即征即退70% 14：即征即退100% 15：超税负3%即征即退 16：超税负8%即征即退 17：超税负12%即征即退 18：超税负6%即征即退
+	FavouredPolicyName  string `json:"favouredPolicyName,omitempty"`  //	N	0	50	增值税特殊管理（优惠政策名称）,当favouredPolicyFlag为1时，此项必填 （数电票时为空）
+	Deduction           string `json:"deduction,omitempty"`           //	N	0	16	扣除额，差额征收时填写，目前只支持填写一项。 注意：当传0、空或字段不传时，都表示非差额征税；传0.00才表示差额征税：0.00 （数电票暂不支持）
+	ZeroRateFlag        string `json:"zeroRateFlag,omitempty"`        //	N	0	1	零税率标识：空,非零税率;1,免税;2,不征税;3,普通零税率；1、当税率为：0%，且增值税特殊管理：为“免税”， 零税率标识：需传“1” 2、当税率为：0%，且增值税特殊管理：为"不征税" 零税率标识：需传“2” 3、当税率为：0%，且增值税特殊管理：为空 零税率标识：需传“3” （数电票时为空）
+	DField1             string `json:"dField1,omitempty"`             //	Y		255	业务方明细自定义字段1，本应用只作保存
+	DField2             string `json:"dField2,omitempty"`             //	Y		255	业务方明细自定义字段2，本应用只作保存
+	DField3             string `json:"dField3,omitempty"`             //	Y		255	业务方明细自定义字段3，本应用只作保存
+	DField4             string `json:"dField4,omitempty"`             //	Y		255	业务方明细自定义字段4，本应用只作保存
+	DField5             string `json:"dField5,omitempty"`             //	Y		255	业务方明细自定义字段5，本应用只作保存
+
 }
 
 type VehicleInfo struct {
